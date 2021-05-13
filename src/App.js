02e00d5a56335188661a91d05f0991dd3b215da2
Joey Sisk <jjmsisk@gmail.com>
2021-05-12 21:01:02 -0500

    added styles to landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { CssBaseline, Typography, AppBar, Card, CardActions, CardContent, CardMedia, Grid, Toolbar, Container, Button } from "@material-ui/core";
 // import { StylesProvider } from '@material-ui/core/styles';
-import "./App.css";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6)
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div>
-      <Typography version="h1">Felans Bakery</Typography>
-
-    </div> 
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6">Felans Bakery</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+              Felans Bakery
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Welcome to my Bakery, please look around at some of my edible art and if you like what you see you can reach out to me and I will make a custom cake to meet your needs.
+            </Typography>
+            <div className={classes.button}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Contact Me
+                  </Button>
+                  <Button variant="outlined" color="primary">
+                    Social Media
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+    </> 
   );
 }
 
