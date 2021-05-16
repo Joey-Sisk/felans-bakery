f059fc11c231c4ac14e55a4fe8fbd15fdbbc02bc
Joey Sisk <jjmsisk@gmail.com>
2021-05-16 00:16:43 -0500

    added filler content for footer

--- src/App.js
+++ src/App.js
@@ -100,6 +100,9 @@ function App() {
         <Typography variant="h6" align="center" gutterBottom>
           Footer
         </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          Somthing here to give the footer a purpose.
+        </Typography>
       </footer>
     </>
   );
