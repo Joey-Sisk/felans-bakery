216114a276d704507d9758ef7337d32cdf6d6c53
Joey Sisk <jjmsisk@gmail.com>
2021-06-03 23:04:05 -0500

    added parallax effect to app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import {
-  CssBaseline,
-} from "@material-ui/core";
+import { CssBaseline } from "@material-ui/core";
 import Footer from "./components/Footer";
 import Landing from "./components/Landing";
 import Cards from "./components/Cards";
 import Navbar from "./components/Navbar";
+import { Parallax } from "react-parallax";
+import parallaxImage from "./images/cupcakes-outside.jpg";
 // import { StylesProvider } from '@material-ui/core/styles';
 
 import useStyles from "./appStyles";
@@ -15,9 +15,17 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <Navbar classes={classes}/>
+      <Navbar classes={classes} />
       <main>
         <Landing classes={classes} />
+        {/* <img src={parallaxImage} alt="sample" /> */}
+        <Parallax
+          bgImage={parallaxImage}
+          bgImageAlt="Cupcakes outside"
+          strength={400}
+        >
+          <h1 style={{ height: "500px" }}>Felans Bakery</h1>
+        </Parallax>
         <Cards classes={classes} />
       </main>
       <Footer classes={classes} />
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import "@fontsource/roboto";
 // import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 // import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
